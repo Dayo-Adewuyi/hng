fa5640637ac5920b9d33871c744a778af9cb50a2
Dayo <dayoadewuyi@protonmail.com>
2022-10-30 13:59:48 +0100

    fix

--- controller/controller.js
+++ controller/controller.js
@@ -2,16 +2,15 @@ const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 
 // url api/info
 exports.info = catchAsyncErrors(async (req, res, next) => {
-    const data = {
+
+
+
+    res.status(200).json({
+
+        success: true,
         slackUsername: "dayo",
         backend: true,
         age: 25,
         bio: "A goal-oriented self starter looking to build the next unicorn"
-    }
-    res.status(200).json({
-
-        success: true,
-        data
-
     });
 });
