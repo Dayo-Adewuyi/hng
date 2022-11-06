06a7bc91273913b59f8d377e06c2f6af55b19044
Dayo <dayoadewuyi@protonmail.com>
2022-11-06 09:31:32 +0100

    task2

--- controller/controller.js
+++ controller/controller.js
@@ -14,3 +14,51 @@ exports.info = catchAsyncErrors(async (req, res, next) => {
         bio: "A goal-oriented self starter looking to build the next unicorn"
     });
 });
+
+exports.operation = catchAsyncErrors(async (req, res, next) => {
+    const{operation_type, x, y} = req.body
+
+    let operation;
+    let result;
+
+    switch(operation_type){
+        case 1:
+            operation = "addition";
+            break;
+        case 2:
+            operation = "subtraction"
+            break;
+        case 3:
+            operation = "multiplication"
+            break;
+        default:
+            operation = "addition"
+
+    }
+
+    if(operation == "addition"){
+       result = x + y
+       return result
+    }
+
+    if(operation == "subtraction"){
+        result = x - y
+        return result
+    }
+    if(operation == "multiplication"){
+        result = x * y
+
+        return result
+    }
+
+    const finalResult = parseInt(result)
+    
+    res.status(200).json({
+
+        success: true,
+        slackUsername: "wande.eth",
+        result: finalResult,
+        operation_type: operation
+      
+    });
+});
--- routes/test.js
+++ routes/test.js
@@ -1,8 +1,9 @@
-const { info } = require("../controller/controller")
+const { info, operation } = require("../controller/controller")
 const express = require('express')
 const router = express.Router()
 
 router.route('/info').get(info)
+router.route('/').post(operation)
 
 
 module.exports = router
